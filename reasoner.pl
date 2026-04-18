63443a20ea7c4395a46703476c6d86f723fe5e81
Miguel Calejo <mc@interprolog.com>
2026-04-18 15:34:46 +0100

    simplify explanations

--- reasoner.pl
+++ reasoner.pl
@@ -26,9 +26,10 @@ i(Goal, SessionModule, Unknowns, Why) :-
     init_counter,
     (   SessionModule:le_my_kb(KBmodule) ->  true;   KBmodule = none
     ),
-    solve(Goal, SessionModule, KBmodule, [], 0, none, Unknowns, Why).
+    solve(Goal, SessionModule, KBmodule, [], 0, none, Unknowns, Whys),
+    (Whys = [Why] -> true ; Why = Whys).
 
-%!  solve(+Goal:term, +SM:atom, +KM:atom, +Anc:list, +Depth:integer, +ParentID:any, -Us:list, -Why:term) is semidet.
+%!  solve(+Goal:term, +SM:atom, +KM:atom, +Anc:list, +Depth:integer, +ParentID:any, -Us:list, -Whys:list) is semidet.
 %
 %   Succeeds if Goal can be proven (possibly with Unknowns).
 %   SM is the Session Module.
@@ -37,85 +38,92 @@ i(Goal, SessionModule, Unknowns, Why) :-
 %   Depth is the current recursion depth.
 %   ParentID is the ID of the calling goal.
 %   Us is the list of Unknowns encountered.
-%   Why is the explanation tree.
-solve(G, SM, KM, Anc, D, ParentID, Us, Why) :-
+%   Whys is a list of "juicy" explanation trees.
+solve(G, SM, KM, Anc, D, ParentID, Us, Whys) :-
     next_id(MyID),
     (   ParentID \== none
     ->  assertz(called(ParentID, MyID, G))
     ;   true
     ),
-    solve_real(G, SM, KM, Anc, D, MyID, Us, Why).
+    solve_real(G, SM, KM, Anc, D, MyID, Us, Whys).
 
 % Conjunction
-solve_real((A, B), SM, KM, Anc, D, MyID, Us, success((A, B), conjunction, [WhyA, WhyB])) :- !,
-    solve(A, SM, KM, Anc, D, MyID, UsA, WhyA),
-    solve(B, SM, KM, Anc, D, MyID, UsB, WhyB),
-    append(UsA, UsB, Us).
-solve_real(and(A, B), SM, KM, Anc, D, MyID, Us, success(and(A, B), conjunction, [WhyA, WhyB])) :- !,
-    solve(A, SM, KM, Anc, D, MyID, UsA, WhyA),
-    solve(B, SM, KM, Anc, D, MyID, UsB, WhyB),
-    append(UsA, UsB, Us).
+solve_real((A, B), SM, KM, Anc, D, MyID, Us, Whys) :- !,
+    solve(A, SM, KM, Anc, D, MyID, UsA, WhysA),
+    solve(B, SM, KM, Anc, D, MyID, UsB, WhysB),
+    append(UsA, UsB, Us),
+    append(WhysA, WhysB, Whys).
+solve_real(and(A, B), SM, KM, Anc, D, MyID, Us, Whys) :- !,
+    solve(A, SM, KM, Anc, D, MyID, UsA, WhysA),
+    solve(B, SM, KM, Anc, D, MyID, UsB, WhysB),
+    append(UsA, UsB, Us),
+    append(WhysA, WhysB, Whys).
 % Disjunction
-solve_real((A ; B), SM, KM, Anc, D, MyID, Us, success((A ; B), disjunction, [Why])) :- !,
-    (   solve(A, SM, KM, Anc, D, MyID, Us, Why)
-    ;   solve(B, SM, KM, Anc, D, MyID, Us, Why)
+solve_real((A ; B), SM, KM, Anc, D, MyID, Us, Whys) :- !,
+    (   solve(A, SM, KM, Anc, D, MyID, Us, Whys)
+    ;   solve(B, SM, KM, Anc, D, MyID, Us, Whys)
     ).
-solve_real(or(A, B), SM, KM, Anc, D, MyID, Us, success(or(A, B), disjunction, [Why])) :- !,
-    (   solve(A, SM, KM, Anc, D, MyID, Us, Why)
-    ;   solve(B, SM, KM, Anc, D, MyID, Us, Why)
+solve_real(or(A, B), SM, KM, Anc, D, MyID, Us, Whys) :- !,
+    (   solve(A, SM, KM, Anc, D, MyID, Us, Whys)
+    ;   solve(B, SM, KM, Anc, D, MyID, Us, Whys)
     ).
 % Aggregates
-solve_real(sum([each, Var], Goal, [Result]), SM, KM, Anc, D, MyID, Us, success(sum([each, Var], Goal, [Result]), aggregate, [WhyGoal])) :- !,
+solve_real(sum([each, Var], Goal, [Result]), SM, KM, Anc, D, MyID, Us, [success(sum([each, Var], Goal, [Result]), aggregate, [WhyGoal])]) :- !,
     D1 is D + 1,
-    findall(Var-Why, solve(Goal, SM, KM, Anc, D1, MyID, [], Why), Pairs),
-    pairs_keys_values(Pairs, List, Whys),
+    findall(Var-Whys, solve(Goal, SM, KM, Anc, D1, MyID, [], Whys), Pairs),
+    pairs_keys_values(Pairs, List, WhysList),
+    flatten(WhysList, WhysGoal),
     sum_list(List, Result),
     Us = [],
-    WhyGoal = success(Goal, aggregate_elements, Whys).
-solve_real(count([each, Var], Goal, [Result]), SM, KM, Anc, D, MyID, Us, success(count([each, Var], Goal, [Result]), aggregate, [WhyGoal])) :- !,
+    WhyGoal = success(Goal, aggregate_elements, WhysGoal).
+solve_real(count([each, Var], Goal, [Result]), SM, KM, Anc, D, MyID, Us, [success(count([each, Var], Goal, [Result]), aggregate, [WhyGoal])]) :- !,
     D1 is D + 1,
-    findall(Var-Why, solve(Goal, SM, KM, Anc, D1, MyID, [], Why), Pairs),
-    pairs_keys_values(Pairs, List, Whys),
+    findall(Var-Whys, solve(Goal, SM, KM, Anc, D1, MyID, [], Whys), Pairs),
+    pairs_keys_values(Pairs, List, WhysList),
+    flatten(WhysList, WhysGoal),
     length(List, Result),
     Us = [],
-    WhyGoal = success(Goal, aggregate_elements, Whys).
-solve_real(min([each, Var], Goal, [Result]), SM, KM, Anc, D, MyID, Us, success(min([each, Var], Goal, [Result]), aggregate, [WhyGoal])) :- !,
+    WhyGoal = success(Goal, aggregate_elements, WhysGoal).
+solve_real(min([each, Var], Goal, [Result]), SM, KM, Anc, D, MyID, Us, [success(min([each, Var], Goal, [Result]), aggregate, [WhyGoal])]) :- !,
     D1 is D + 1,
-    findall(Var-Why, solve(Goal, SM, KM, Anc, D1, MyID, [], Why), Pairs),
-    pairs_keys_values(Pairs, List, Whys),
+    findall(Var-Whys, solve(Goal, SM, KM, Anc, D1, MyID, [], Whys), Pairs),
+    pairs_keys_values(Pairs, List, WhysList),
+    flatten(WhysList, WhysGoal),
     min_list(List, Result),
     Us = [],
-    WhyGoal = success(Goal, aggregate_elements, Whys).
-solve_real(max([each, Var], Goal, [Result]), SM, KM, Anc, D, MyID, Us, success(max([each, Var], Goal, [Result]), aggregate, [WhyGoal])) :- !,
+    WhyGoal = success(Goal, aggregate_elements, WhysGoal).
+solve_real(max([each, Var], Goal, [Result]), SM, KM, Anc, D, MyID, Us, [success(max([each, Var], Goal, [Result]), aggregate, [WhyGoal])]) :- !,
     D1 is D + 1,
-    findall(Var-Why, solve(Goal, SM, KM, Anc, D1, MyID, [], Why), Pairs),
-    pairs_keys_values(Pairs, List, Whys),
+    findall(Var-Whys, solve(Goal, SM, KM, Anc, D1, MyID, [], Whys), Pairs),
+    pairs_keys_values(Pairs, List, WhysList),
+    flatten(WhysList, WhysGoal),
     max_list(List, Result),
     Us = [],
-    WhyGoal = success(Goal, aggregate_elements, Whys).
-solve_real(average([each, Var], Goal, [Result]), SM, KM, Anc, D, MyID, Us, success(average([each, Var], Goal, [Result]), aggregate, [WhyGoal])) :- !,
+    WhyGoal = success(Goal, aggregate_elements, WhysGoal).
+solve_real(average([each, Var], Goal, [Result]), SM, KM, Anc, D, MyID, Us, [success(average([each, Var], Goal, [Result]), aggregate, [WhyGoal])]) :- !,
     D1 is D + 1,
-    findall(Var-Why, solve(Goal, SM, KM, Anc, D1, MyID, [], Why), Pairs),
-    pairs_keys_values(Pairs, List, Whys),
+    findall(Var-Whys, solve(Goal, SM, KM, Anc, D1, MyID, [], Whys), Pairs),
+    pairs_keys_values(Pairs, List, WhysList),
+    flatten(WhysList, WhysGoal),
     sum_list(List, Sum),
     length(List, Count),
     (Count > 0 -> Result is Sum / Count ; Result = 0),
     Us = [],
-    WhyGoal = success(Goal, aggregate_elements, Whys).
+    WhyGoal = success(Goal, aggregate_elements, WhysGoal).
 % Forall
-solve_real(forall(Cond, Cons), SM, KM, Anc, D, MyID, Us, success(forall(Cond, Cons), universal, [Why])) :- !,
+solve_real(forall(Cond, Cons), SM, KM, Anc, D, MyID, Us, [success(forall(Cond, Cons), universal, Whys)]) :- !,
     D1 is D + 1,
-    findall(UsC-WhyC, solve(Cond, SM, KM, Anc, D1, MyID, UsC, WhyC), CondResults),
-    (   CondResults == [] -> Us = [], Why = success(Cond, empty_forall, [])
+    findall(UsC-WhysC, solve(Cond, SM, KM, Anc, D1, MyID, UsC, WhysC), CondResults),
+    (   CondResults == [] -> Us = [], Whys = [success(Cond, empty_forall, [])]
     ;   % For each solution of Cond, Cons must succeed
-        forall(member(UsC-WhyC, CondResults),
+        forall(member(UsC-WhysC, CondResults),
                (UsC == [] -> solve(Cons, SM, KM, Anc, D1, MyID, [], _) ; true)),
         Us = [], % TODO: handle unknowns in forall
-        Why = success(forall(Cond, Cons), universal_success, [])
+        Whys = [success(forall(Cond, Cons), universal_success, [])]
     ).
 
 % Negation as Failure
-solve_real(not(Goal), SM, KM, Anc, D, MyID, Us, success(not(Goal), negation, [FailureTree])) :- !,
+solve_real(not(Goal), SM, KM, Anc, D, MyID, Us, [success(not(Goal), negation, FailureTrees)]) :- !,
     D1 is D + 1,
     next_id(GoalID),
     assertz(called(MyID, GoalID, Goal)),
@@ -124,32 +132,31 @@ solve_real(not(Goal), SM, KM, Anc, D, MyID, Us, success(not(Goal), negation, [Fa
     ->  fail % Certain success of Goal, so not(Goal) fails
     ;   AllUsA \== []
     ->  Us = [not(Goal)], % Only unknown successes
-        build_failure_tree(GoalID, FailureTree)
+        build_failure_tree(GoalID, FailureTrees)
     ;   Us = [], % Certain failure of Goal, so not(Goal) succeeds
-        build_failure_tree(GoalID, FailureTree)
+        build_failure_tree(GoalID, FailureTrees)
     ).
 
 % True
-solve_real(true, _, _, _, _, _, [], success(true, built_in, [])) :- !.
+solve_real(true, _, _, _, _, _, [], []) :- !.
 
 % Literals
-solve_real(G, SM, KM, Anc, D, MyID, Us, Why) :-
+solve_real(G, SM, KM, Anc, D, MyID, Us, [success(G, Ref, WhysBody)]) :-
     G \= (_ , _), G \= (_ ; _), G \= and(_, _), G \= or(_, _), G \= not(_), G \= true, G \= fail,
     G \= sum(_, _, _), G \= count(_, _, _), G \= min(_, _, _), G \= max(_, _, _), G \= average(_, _, _),
     (   D > 100 -> fail ; true % Depth limit
     ),
 
     (   KM \== none, current_predicate(KM:le_unknown/1), KM:le_unknown(G)
-    ->  Us = [G], Why = success(G, unknown, [])
+    ->  Us = [G], WhysBody = [success(G, unknown, [])]
     ;   is_built_in(G)
-    ->  call_reasoner_built_in(G), Us = [], Why = success(G, built_in, [])
+    ->  call_reasoner_built_in(G), Us = [], Ref = built_in, WhysBody = []
     ;   G = is_a(X, Z)
     ->  D1 is D + 1,
         (   get_clause(is_a(X, Z), SM, KM, Body, Ref),
             \+ SM:le_neg(is_a(X, Z)),
             \+ member(is_a(X, Z), Anc),
-            solve(Body, SM, KM, [is_a(X, Z)|Anc], D1, MyID, Us, WhyBody),
-            Why = success(is_a(X, Z), Ref, [WhyBody])
+            solve(Body, SM, KM, [is_a(X, Z)|Anc], D1, MyID, Us, WhysBody)
         ;   % Transitivity: X is a Y and Y is a Z
             % Use a base fact for the first step to avoid infinite recursion
             (SM:clause(is_a(X, Y), true, Ref1) ; (KM \== none, KM:clause(is_a(X, Y), true, Ref1))),
@@ -159,22 +166,32 @@ solve_real(G, SM, KM, Anc, D, MyID, Us, Why) :-
             % Record the fact call
             next_id(FactID),
             assertz(called(MyID, FactID, is_a(X, Y))),
-            solve(is_a(Y, Z), SM, KM, [is_a(X, Z)|Anc], D1, MyID, Us, WhyBody2),
-            Why = success(is_a(X, Z), transitivity, [success(is_a(X, Y), Ref1, []), WhyBody2])
+            solve(is_a(Y, Z), SM, KM, [is_a(X, Z)|Anc], D1, MyID, Us, WhysBody2),
+            Ref = transitivity,
+            WhysBody = [success(is_a(X, Y), Ref1, []) | WhysBody2]
         )
     ;   get_clause(G, SM, KM, Body, Ref),
         \+ SM:le_neg(G),
         \+ member(G, Anc),
         D1 is D + 1,
-        solve(Body, SM, KM, [G|Anc], D1, MyID, Us, WhyBody),
-        Why = success(G, Ref, [WhyBody])
+        solve(Body, SM, KM, [G|Anc], D1, MyID, Us, WhysBody)
     ).
 
-% build_failure_tree(+ID, -Tree)
-% Reconstructs a tree of all calls made under ID.
-build_failure_tree(ID, failure(Term, Children)) :-
-    (called(_, ID, Term) -> true ; Term = unknown),
-    findall(ChildTree, (called(ID, ChildID, _), build_failure_tree(ChildID, ChildTree)), Children).
+% build_failure_tree(+ID, -Whys)
+% Reconstructs a list of "juicy" failure trees of all calls made under ID.
+build_failure_tree(ID, Whys) :-
+    called(_, ID, Term),
+    (   is_trivial(Term)
+    ->  findall(W, (called(ID, CID, _), build_failure_tree(CID, Ws), member(W, Ws)), Whys)
+    ;   findall(W, (called(ID, CID, _), build_failure_tree(CID, Ws), member(W, Ws)), Children),
+        Whys = [failure(Term, Children)]
+    ).
+
+is_trivial((_, _)).
+is_trivial(and(_, _)).
+is_trivial((_ ; _)).
+is_trivial(or(_, _)).
+is_trivial(true).
 
 % get_clause(+Goal, +SM, +KM, -Body, -Ref)
 get_clause(G, SM, _KM, Body, Ref) :-
